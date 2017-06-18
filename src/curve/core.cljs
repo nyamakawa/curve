3b9d08be890ed9f9c5286ea223a1bc9be1e62f9a
nyamakawa <nyamakawa2@gmail.com>
2017-06-18 15:12:57 +0900

    add draw-pixel-colo

--- src/curve/core.cljs
+++ src/curve/core.cljs
@@ -27,11 +27,43 @@
     canvas-dom))
 
 (defn draw-pixel [canvas data x y r g b & a]
+  (println x y r g b (first a))
   (let [offset (+ (* x 4) (* y (.-height canvas) 4))]
     (aset data offset r)
     (aset data (+ offset 1) g)
     (aset data (+ offset 2) b)
-    (aset data (+ offset 3) (or a 255)))
+    (aset data (+ offset 3) (or (first a) 255)))
+  data)
+
+(defn draw-pixel-color [canvas data x y color]
+  (let [length (count color)]
+    (println length)
+    (println (nth color 1))
+    (cond
+      (= length 1)
+      (draw-pixel canvas data x y
+                  (nth color 0)
+                  (nth color 0)
+                  (nth color 0)
+                  255)
+      (= length 2)
+      (draw-pixel canvas data x y
+                  (nth color 0)
+                  (nth color 0)
+                  (nth color 0)
+                  (nth color 1))
+      (= length 3)
+      (draw-pixel canvas data x y
+                  (nth color 0)
+                  (nth color 1)
+                  (nth color 2)
+                  255)
+      (= length 4)
+      (draw-pixel canvas data x y
+                  (nth color 0)
+                  (nth color 1)
+                  (nth color 2)
+                  (nth color 3))))
   data)
 
 (defn draw [canvas]
@@ -41,6 +73,7 @@
         image (.createImageData ctx canvas-width canvas-height)
         data (.-data image)]
     (draw-pixel canvas data 0 0 0 0 0) ;test
+    (draw-pixel-color canvas data 20 20 [255 0 0]) ;test    
     (.putImageData ctx image 0 0)))
   
 (draw (init))
